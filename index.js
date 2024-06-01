b24773df6e76948873524c956c1a5ba875caf0ea
Aon Mao <aonmao888@gmail.com>
2024-06-01 14:05:21 +0800

    '1.2'

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const {createClient} = require('@supabase/supabase-js')
+const cors = require('cors');
 
 const supabase = createClient('https://walwibbnedmfzzhqtuqz.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhbHdpYmJuZWRtZnp6aHF0dXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAzNjgxMDUsImV4cCI6MjAxNTk0NDEwNX0.c0CCF50vux3GKvR70ONdZ4E-yjfLu_vgazK0k-LZyN0');
+
+app.use(cors({
+    origin:'*'
+}));
 
 app.get('/',(req,res)=>{
     res.send('Home')
